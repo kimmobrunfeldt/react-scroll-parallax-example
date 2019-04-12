5e8afb56ca9b4df4654019cd80b71f4702a22cf3
Kimmo Brunfeldt <kimmo@aito.ai>
2019-04-12 13:19:22 +0300

    Add example parallax code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Parallax } from 'react-scroll-parallax'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Parallax y={["0px", "100px"]}>
+            <img src={logo} className="App-logo" alt="logo" />
+          </Parallax>
+
           <a
             className="App-link"
             href="https://reactjs.org"
